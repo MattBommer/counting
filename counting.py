babf7f670878be43dab616da5f1443481725bab2
matt bommer <mbommer@utexas.edu>
2020-05-15 15:06:13 -0500

    basic strat and ace value lowering added

--- counting.py
+++ counting.py
@@ -1,5 +1,4 @@
 import sys
-import collections
 
 """
 This list is basic strategy for hitting and staying on hard hands (hands without aces) in blackjack.
@@ -58,6 +57,8 @@ pair_splitting = [["Y/N", "Y/N", "Y", "Y", "Y", "Y", "N", "N", "N", "N"],
                   ["Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y", "Y"]]
 
 
+interpretation  = {"H" : "hit", "S" : "stay", "D" : "double down", "Y" : "split the pair", "N" : "DON'T split the pair", "Y/N" : "split only if you can double down after"}
+
 #Args: 
 #   0 : The number of decks being used for the shoe (usually 6 or 8), 
 #   1 : The number of players at the table besides yourself
@@ -70,9 +71,9 @@ def main(args):
     except ValueError:
         print("One or more arguements is not of type int")
         sys.exit()
-    cc = Count(deck_num, player_position)
+    cc = Count(deck_num)
     cc.add_players(player_num)
-    cc.place_the_player()
+    cc.place_the_player(player_position)
     cc.run_round()
 	
 
@@ -96,11 +97,11 @@ class Count:
     def remove_player(self, pos):
         self.players.pop(pos)
 
-	def calculate_count(self):
-		for x in self.players:
-			hand = x.get_hand()
-			self.card_count += hand.ret_count()
-			self.card_num += hand.num_cards()
+    def running_count(self):
+    	for x in self.players:
+    		hand = x.get_hand()
+    		self.card_count += hand.ret_count()
+    		self.card_num += hand.num_cards()
 
     def run_round(self):
         for l in range(2):
@@ -113,20 +114,35 @@ class Count:
                 x.hand_dealt(val)
             if l < 1:
                 self.dealer.hand_dealt(input("Dealers upcard: ").strip().replace(" ",""))
-        for p in self.players:
+        upidx = self.dealer.get_hand().hand_value() - 2
+        for i,p in enumerate(self.players):
             curr_hand = p.get_hand()
-            total = curr_hand.card_val()
-            stay = False
-            while (total <= 21 or stay):
+            total = curr_hand.hand_value()
+            stay = True
+            print("Player " + i + " turn.")
+            while (total < 21 and stay):
                 if p.is_the_player:
-                    #go into the tables above to choose the right statistical move and inform the player
-                    pass
+                    self.basic_strategy(curr_hand, total, upidx)
                 val = input("Next card (use s to indicate the player is staying): ").strip().replace(" ","")
-                stay = True if val.lower() == "s" else False
-                if not stay:
+                stay = False if val.lower() == "s" else True
+                if stay:
                     curr_hand.hand_dealt(val)
-                    total = curr_hand.card_val()
+                    total = curr_hand.hand_value()
 
+    def basic_strategy(self, curr_hand, total, upidx):
+        print("It\'s your turn now!")
+        if curr_hand.is_pair:
+            print(interpretation[pair_splitting[curr_hand.get_card(0).get_value() - 2][upidx]])
+            #Implement splitting option where player has more than one hand
+        elif curr_hand.ace():
+            print(interpretation[soft_totals[total - 13][upidx]])
+        else:
+            if total > 17:
+                print("stay")
+            elif total < 8:
+                print("hit")
+            else:
+                print(interpretation[hard_totals[total - 8][upidx]])
 
 class Player:
     def __init__ (self, the_player=False):
@@ -163,27 +179,37 @@ class Hand:
     def ret_count(self):
         return self.hand_count
 
+    #Case of pair of aces is not taken care of
     def update_hand(self):
         if self.idx < len(self.cards):
             for x in self.cards[self.idx:]:
                 self.hand_count += x.get_count()
                 self.value += x.get_value()
-                self.has_ace = x.is_ace()
+                if not self.has_ace:
+                    self.has_ace = x.is_ace()
             if self.idx < 2:
-                self.pair = self.cards[0].is_pair(self.cards[0])
+                self.pair = self.cards[0].is_pair(self.cards[1])
             self.idx = len(self.cards)
+        if self.value > 21 and self.has_ace:
+            for x in self.cards:
+                if x.is_ace and x.get_value != 1:
+                    x.set_ace()
+                    break
+
+    def get_card(self, idx):
+        return self.cards[idx]
 
     def ace(self):
         return self.has_ace
 
-    def pair(self):
+    def is_pair(self):
         return self.pair
 
-    def card_value(self):
+    def hand_value(self):
         return self.value
 
-	def num_cards(self):
-		return len(self.cards)
+    def num_cards(self):
+    	return len(self.cards)
 
 class Card:
 
@@ -191,10 +217,11 @@ class Card:
 
     def __init__ (self, val):
         self.card_num = str(val)
+        self.card_val = 0
         try:
             self.card_val += int(val)
         except ValueError:
-            self.card_val += card[val.upper()]
+            self.card_val += self.card[val.upper()]
 
         if self.card_val >= 10:
             self.count_val = 1
@@ -209,8 +236,11 @@ class Card:
     def get_count (self):
         return self.count_val
 
+    def set_ace(self):
+        self.card_val = 1
+
     def get_value(self):
-        return self.card_value
+        return self.card_val
 
     def get_num(self):
         return self.card_num
